b505c9ccbc938eff16419c0b7c4eac05e9ef3fe7
LuDaShi <wlty17173@gmail.com>
2023-11-12 21:19:43 +0800

    build

--- avwq_build/build_jdk21.sh
+++ avwq_build/build_jdk21.sh
@@ -1,14 +1,24 @@
+cd ..
 # 赋予可执行权限
 chmod +x configure
 
+sudo rm /usr/lib64/ccache/gcc
+sudo rm /usr/lib64/ccache/g++
 # 有很多配置项，可以参看《深入理解JAVA虚拟机》，这里不说深入介绍。
-./configure
+bash configure --enable-ccache --with-boot-jdk=/software/programFiles/jdk-21_linux-x64_bin/jdk-21.0.1  --build=x86_64-linux-gnu 
 
 # 多次编译，清除之前的数据
 make clean
 
+
 # 编译
-make
+make images
+
+# 验证jdk
+./build/*/images/jdk/bin/java -version
+
+# 运行测试
+make run-test-tier1
 
 # 结果
 # Finished building target 'default (exploded-image)' in configuration 'linux-x86_64-server-release'
